5f8190172b270200f753c3caacd60813fab79798
lailah <69699864+lnabegu@users.noreply.github.com>
2021-07-29 14:20:03 -0400

    Update Environment.py
    updated (somewhat) working code

--- Environment.py
+++ Environment.py
@@ -2,9 +2,12 @@ from adafruit_clue import clue
 import adafruit_sgp30
 import time
 import board
+import displayio
+
+clue_display = clue.simple_text_display(text_scale=1, colors=(clue.WHITE,))
 
 sgp30 = adafruit_sgp30.Adafruit_SGP30(board.I2C())
-    time.sleep(15)
+time.sleep(15)
 
 while True:
     print("Pressure: {:.3f} hPa".format(clue.pressure))
@@ -13,7 +16,9 @@ while True:
     print("Humidity: {:.1f} %".format(clue.humidity))
 
     eCO2, TVOC = sgp30.iaq_measure()
-    tvoc.text = "TVOC={:5d}".format(TVOC)
-    eco2.text = "eCO2={:5d}".format(eCO2)
+    print(eCO2)
+    print(TVOC)
+    clue_display[0].text = "TVOC={:5d}".format(TVOC)
+    clue_display[1].text = "eCO2={:5d}".format(eCO2)
     print("--------------------------------")
 
