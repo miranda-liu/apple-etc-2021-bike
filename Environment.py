ac661500a68f4e9d595d6a3eaf5f9f51cd2b415e
lailah <69699864+lnabegu@users.noreply.github.com>
2021-07-29 15:59:13 -0400

    Update Environment.py
    working code!!!

--- Environment.py
+++ Environment.py
@@ -4,21 +4,21 @@ import time
 import board
 import displayio
 
-clue_display = clue.simple_text_display(text_scale=1, colors=(clue.WHITE,))
+clue_display = clue.simple_text_display(title="SPG30 Sensor!", title_scale=2)
 
 sgp30 = adafruit_sgp30.Adafruit_SGP30(board.I2C())
-time.sleep(15)
+time.sleep(5)
 
 while True:
-    print("Pressure: {:.3f} hPa".format(clue.pressure))
-    print("Altitude: {:.1f} m".format(clue.altitude))
-    print("Temperature: {:.1f} C".format(clue.temperature))
-    print("Humidity: {:.1f} %".format(clue.humidity))
-
     eCO2, TVOC = sgp30.iaq_measure()
     print(eCO2)
     print(TVOC)
-    clue_display[0].text = "TVOC={:5d}".format(TVOC)
-    clue_display[1].text = "eCO2={:5d}".format(eCO2)
-    print("--------------------------------")
+    
+    clue_display[0].text = "Pressure: {:.3f} hPa".format(clue.pressure)
+    clue_display[1].text = "Altitude: {:.1f} m".format(clue.altitude)
+    clue_display[2].text = "Temperature: {:.1f} C".format(clue.temperature)
+    clue_display[3].text = "Humidity: {:.1f} %".format(clue.humidity)
+    clue_display[4].text = "TVOC={:5d}".format(TVOC)
+    clue_display[5].text = "eCO2={:5d}".format(eCO2)
+    clue_display.show()
 
